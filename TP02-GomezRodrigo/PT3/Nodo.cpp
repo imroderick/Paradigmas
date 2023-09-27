c5787ba897f9d0a32b7e04c5c73cf50c708e0b6c
Unagui19 <francorr199@gmail.com>
2023-09-27 17:05:27 -0300

    funcion enfila y defila + cambios

--- TP02-GomezRodrigo/PT3/Nodo.cpp
+++ TP02-GomezRodrigo/PT3/Nodo.cpp
@@ -1,16 +1,5 @@
 #include "Nodo.h"
 
-Nodo::Nodo(item valor)
-{
-    this->dato = valor;
-    siguiente = nullptr;
-}
+Nodo::Nodo(item valor) : dato(valor), siguiente(nullptr){}
 
-Nodo::~Nodo()
-{
-}
-
-item Nodo::Dato()
-{
-    return this->dato;
-}
+Nodo::~Nodo(){}
